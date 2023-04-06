37ca4f8a462f4b673a88102112668a5bd0403b37
Fernando Gabriel <fernandog_briel@hotmail.com>
2023-04-06 20:29:30 -0300

    v1

--- main.py
+++ main.py
@@ -55,31 +55,56 @@ dot_data = tree.export_graphviz(clf, out_file=None,  feature_names=columns,
                                 class_names=classes,  filled=True,
                                 rounded=True, special_characters=True) 
 graph = pydotplus.graph_from_dot_data(dot_data)
-#graph.write_pdf("titanic.pdf")
+#graph.write_pdf("wine.pdf")
 graph.write_png('wine.png')
 
 
 # Predict using our model
 
-#14.23,1.71,2.43,15.6,127,2.8,3.06,0.28,2.29,5.64,1.04,3.92,1065   Wine Type 1
- 
-#12.37,0.94,1.36,10.6,88,1.98,0.57,0.28,0.42,1.95,1.05,1.82,520    Wine Type 2
+# Wine Type 1:
+#Alcohol                     = 14.23
+#Malicacid                   = 1.71
+#Ash                         = 2.43
+#Alcalinityofash             = 15.6
+#Magnesium                   = 127
+#Totalphenols                = 2.8
+#Flavanoids                  = 3.06
+#Nonflavanoidphenols         = 0.28
+#Proanthocyanins             = 2.29
+#Colorintensity              = 5.64
+#Hue                         = 1.04
+#OD280andOD315ofdilutedwines = 3.92
+#Proline                     = 1065
 
-#12.86,1.35,2.32,18,122,1.51,1.25,0.21,0.94,4.1,0.76,1.29,630      Wine Type 3
+# Wine Type 2:
+#Alcohol                     = 12.37
+#Malicacid                   = 0.94
+#Ash                         = 1.36
+#Alcalinityofash             = 10.6
+#Magnesium                   = 88
+#Totalphenols                = 1.98
+#Flavanoids                  = 0.57
+#Nonflavanoidphenols         = 0.28
+#Proanthocyanins             = 0.42
+#Colorintensity              = 1.95
+#Hue                         = 1.05
+#OD280andOD315ofdilutedwines = 1.82
+#Proline                     = 520
 
-Alcohol = 12.86
-Malicacid = 1.35
-Ash = 2.32
-Alcalinityofash = 18
-Magnesium = 122
-Totalphenols = 1.51
-Flavanoids = 1.25
-Nonflavanoidphenols = 0.21
-Proanthocyanins = 0.94
-Colorintensity = 4.1
-Hue = 0.76
+# Wine Type 3:
+Alcohol                     = 12.86
+Malicacid                   = 1.35
+Ash                         = 2.32
+Alcalinityofash             = 18
+Magnesium                   = 122
+Totalphenols                = 1.51
+Flavanoids                  = 1.25
+Nonflavanoidphenols         = 0.21
+Proanthocyanins             = 0.94
+Colorintensity              = 4.1
+Hue                         = 0.76
 OD280andOD315ofdilutedwines = 1.29
-Proline = 630
+Proline                     = 630
 
 # Predict a single decision, type 1, 2 or 3?
 print(clf.predict([[Alcohol,
